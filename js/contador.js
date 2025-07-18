f3ffb230e3fd8cb284fd34b4b2aaa0a29cc022ce
BryanX309 <bryanesaucruz2002@gmail.com>
2025-07-17 19:33:49 -0600

    Segundo ejercicio

--- js/contador.js
+++ js/contador.js
@@ -1,1 +1,16 @@
-console.log('Contador');
+const contador = document.querySelector('#counter');
+
+const aumentar = document.querySelector('#aumentar');
+const disminiuir = document.querySelector('#disminiuir');
+
+aumentar.addEventListener('click', ()=>{
+    let n = parseInt(contador.textContent);
+    n++;
+    contador.textContent = n;
+})
+
+disminiuir.addEventListener('click', ()=>{
+    let n = parseInt(contador.textContent);
+    n--;
+    contador.textContent = n;
+})
