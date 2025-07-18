fbac660cabef1eff12e5c663bc95898887686d5f
BryanX309 <bryanesaucruz2002@gmail.com>
2025-07-17 19:49:35 -0600

    Tercer ejercicio

--- js/lista-tareas.js
+++ js/lista-tareas.js
@@ -1,1 +1,16 @@
-console.log('Lista de Tareas');
+const BtnAgregar = document.querySelector('#agregar');
+
+
+BtnAgregar.addEventListener('click', ()=>{
+
+    const nuevaTarea = document.querySelector('#taskInput').value || '';
+    const lista = document.querySelector('#taskList');
+
+    if(nuevaTarea !== ''){
+        const tarea = `<li>${nuevaTarea}</li>\n`;
+        lista.innerHTML = lista.innerHTML.concat(tarea);
+    }
+    else{
+        alert('No se puede ingresar tareas vacías');        
+    }
+})
